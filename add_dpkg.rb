5578b8474bf3dfd21761e7bf7ecaaf715c4084a7
nirnanaaa <mosny@zyg.li>
2014-09-22 15:18:49 +0200

    updated webrunner for apt

--- add_dpkg.rb
+++ add_dpkg.rb
@@ -5,5 +5,6 @@ require 'yaml'
 if File.exist?("/data/config/additional_packages.yml")
   packages = YAML.load_file("/data/config/additional_packages.yml")
   inst = packages["install"].join(" ")
+  `apt-get update -yqq`
   `apt-get install #{inst} -yqq`
 end
